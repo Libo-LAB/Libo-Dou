913cb7e1c19a35b1e628947aea9d2add11ca4a82
ldou <dlb317189070@outlook.com>
2020-08-25 22:23:53 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,21 @@
+/*
+ *********
+ * Author: Libo Dou
+ *         ldou3@huskers.unl.edu
+ * Date: 2020/8/23
+ *
+ * A simple hello world program in C
+ **********
+ */
+
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv)
+ {
+ 
+    printf("this is my first program!\n");
+ 
+return 0;
+    
+  }
